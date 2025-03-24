a542f09d46878a58d858ce6ed17660f2f6e5220b
Yan <yanfelixpimentell@gmail.com>
2025-03-24 11:15:44 -0300

    new commit

--- JavaBasico/src/Operadores.java
+++ JavaBasico/src/Operadores.java
@@ -10,15 +10,25 @@ public class Operadores {
       
       // System.out.println(v);
 
-      int numero = 10;
-      int numero2 = 6;
+      // int numero = 10;
+      // int numero2 = 6;
 
 
-      // TERNARIOS
-      String resultado = numero > numero2 ? "Verdadeiro" : "Falso";
+      // // TERNARIOS
+      // String resultado = numero > numero2 ? "Verdadeiro" : "Falso";
 
-      System.out.println(resultado);
-        
-        
+      // System.out.println(resultado);
+
+      //OPERADORES LOGICOS
+      boolean condicao1 = true;
+      boolean condicao2 = false;
+
+      if(condicao1 && condicao2){
+        System.out.println("Verdadeiro");
     }
+
+      if (condicao1 || condicao2) {
+        System.out.println("Uma das condições é verdadeira");
+      }
+}
 }
