2583574e29878d3fff59b35b3582c7c475c34c5d
Yan <yanfelixpimentell@gmail.com>
2025-03-24 11:09:15 -0300

    new commit

---
+++ JavaBasico/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+
+      boolean v = false;
+      
+      v = !v;
+      
+      System.out.println(v);
+        
+        
+    }
+}
